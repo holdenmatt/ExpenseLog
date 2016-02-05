2f29fbe075ea4aee33e93708bfba2a9a3a26d0e8
Matt Holden <holden.matt@gmail.com>
2016-02-05 12:44:05 +0700

    Remove src/ folder

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn src.server.app:app --log-file -
+web: gunicorn server.app:app --log-file -
--- server/config.py
+++ server/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     DEBUG = os.environ['DEBUG']
 
-    STATIC_FOLDER = '../../build'
+    STATIC_FOLDER = '../build'
 
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
