5cd6a9c879c5dcf4aa07a1fbd4769f32af347b80
Matt Holden <holden.matt@gmail.com>
2016-02-06 19:38:11 +0700

    Add server tests

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ from config import Config
 
 app = Flask(__name__, static_folder=Config.STATIC_FOLDER)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+db = app.db = SQLAlchemy(app)
 
 class Summary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
---
+++ server/test_app.py
@@ -0,0 +1,145 @@
+import json
+import unittest
+from nose.tools import *
+from app import app
+
+
+def check_valid_json(res):
+    eq_(res.headers['Content-Type'], 'application/json')
+    content = json.loads(res.data)
+    return content
+
+
+class AppTests(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.db.drop_all()
+        app.db.create_all()
+        self.app = app.test_client()
+
+    def post_json(self, url, data):
+        data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        return self.app.post(url, data=data, headers=headers)
+
+    def put_json(self, url, data):
+        data = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+        return self.app.put(url, data=data, headers=headers)
+
+    def test_summary(self):
+        # Check we start empty.
+        res = self.app.get('/api/summary')
+        eq_(res.status_code, 200)
+        content = check_valid_json(res)
+        eq_(content['num_results'], 0)
+        eq_(content['objects'], [])
+
+        # Create a summary.
+        res = self.post_json('/api/summary', data={
+            'date': '2016-01-01',
+            'summary': 'some text'
+        })
+        eq_(res.status_code, 201)
+        content = check_valid_json(res)
+        eq_(content['id'], 1)
+        eq_(content['date'], '2016-01-01')
+        eq_(content['summary'], 'some text')
+
+        # Modify it.
+        res = self.put_json('/api/summary/1', data={
+            'summary': 'new summary'
+        })
+        eq_(res.status_code, 200)
+
+        # Get it.
+        res = self.app.get('/api/summary/1')
+        eq_(res.status_code, 200)
+        content = check_valid_json(res)
+        eq_(content['summary'], 'new summary')
+
+        # Get all.
+        res = self.app.get('/api/summary')
+        eq_(res.status_code, 200)
+        content = check_valid_json(res)
+        eq_(content['num_results'], 1)
+        eq_(content['objects'][0]['summary'], 'new summary')
+
+        # Delete it.
+        res = self.app.delete('/api/summary/1')
+        eq_(res.status_code, 204)
+
+        # Check we're empty again.
+        res = self.app.get('/api/summary')
+        content = check_valid_json(res)
+        eq_(content['num_results'], 0)
+
+    def test_summary_errors(self):
+        res = self.post_json('/api/summary', data={
+            'foo': 'bar'
+        })
+        eq_(res.status_code, 400)
+
+        res = self.post_json('/api/summary', data={
+            'date': '2016-01-01',
+            'summary': 'some text'
+        })
+        eq_(res.status_code, 201)
+
+        res = self.post_json('/api/summary', data={
+            'date': '2016-01-01',
+            'summary': 'duplicate date'
+        })
+        eq_(res.status_code, 400)
+        content = check_valid_json(res)
+        eq_(content, {"message": "IntegrityError"})
+
+    def test_expense(self):
+        # Check we start empty.
+        res = self.app.get('/api/expense')
+        eq_(res.status_code, 200)
+        content = check_valid_json(res)
+        eq_(content['num_results'], 0)
+        eq_(content['objects'], [])
+
+        # Create an expense.
+        res = self.post_json('/api/expense', data={
+            'date': '2016-01-01',
+            'tag': 'Food',
+            'currency': 'USD',
+            'amt': 100,
+            'desc': 'dinner and beers'
+        })
+        eq_(res.status_code, 201)
+        content = check_valid_json(res)
+        eq_(content['id'], 1)
+        eq_(content['amt'], 100)
+
+        # Modify not allowed.
+        res = self.put_json('/api/expense/1', data={
+            'amt': 200
+        })
+        eq_(res.status_code, 405)
+
+        # Get it.
+        res = self.app.get('/api/expense/1')
+        eq_(res.status_code, 200)
+        content = check_valid_json(res)
+        eq_(content['amt'], 100)
+
+        # Get all.
+        res = self.app.get('/api/expense')
+        eq_(res.status_code, 200)
+        content = check_valid_json(res)
+        eq_(content['num_results'], 1)
+        eq_(content['objects'][0]['amt'], 100)
+
+        # Delete it.
+        res = self.app.delete('/api/expense/1')
+        eq_(res.status_code, 204)
+
+        # Check we're empty again.
+        res = self.app.get('/api/expense')
+        content = check_valid_json(res)
+        eq_(content['num_results'], 0)
